4cf091a98d715a16c8ccc3a42bc4db993c71c4ae
mercen1 <nurhabib102@gmail.com>
2020-03-05 15:44:36 +0700

    tambah dan edit

--- exercise-2.js
+++ exercise-2.js
@@ -4,18 +4,15 @@
 
 // console.log(target);
 
-function Balikkata(kata){ 
-    var balik =''
-    for( i=1 ; i<=kata.length ; i++) {
-    
-    balik+= kata[kata.length-i]
-        
+function Balikkata(kata) {
+    var balik = ''
+    for (i = 1; i <= kata.length; i++) {
+
+        balik += kata[kata.length - i]
+
     }
     return balik;
 }
-var warna =['hijau','merah','hitam']
+var warna = ['hijau', 'merah', 'hitam']
 console.log(Balikkata(warna));
 console.log(Balikkata('Hello dunia'))
-
-
-
