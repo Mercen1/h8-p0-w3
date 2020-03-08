9a2c713815b46e6d92c3137e297616ab4925c831
mercen1 <nurhabib102@gmail.com>
2020-03-08 21:26:19 +0700

    tambah latihan1

---
+++ lewatiLembah.js
@@ -0,0 +1,19 @@
+/* 
+Ninja Hatori adalah pendaki gunung yang rajin. Dia mencatat naik turun gunungnya dengan cermat, memperhatikan detail kecil seperti topografi. Untuk setiap langkah yang dia ambil, dia mencatat 
+apakah itu langkah menanjak atau menurun. Langkah Ninja Hatori selalu dimulai dan berakhir di permukaan laut.
+Berikut ini beberapa istilah yang dipakai:
+    Gunung adalah urutan langkah-langkah berurutan di atas permukaan laut, dimulai dengan langkah naik dari permukaan laut dan berakhir dengan langkah turun ke permukaan laut.
+    Lembah adalah urutan langkah-langkah berurutan di bawah permukaan laut, dimulai dengan langkah turun dari permukaan laut dan berakhir dengan langkah naik ke permukaan laut.
+N = naik
+T = turun
+
+Hitung berapa jumlah lembah yang dilaluinya.
+*/
+
+function jumlahLembah(s) {
+  
+    return ;
+}
+
+console.log(jumlahLembah(['N', 'T', 'T','T','U','T','U','U'])); // 1
+console.log(jumlahLembah(['T','T','N','N','T','T','N','T','N','N','N','T'])); // 2
